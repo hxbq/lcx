510645f7f7555e2d286eb0f867efac16d2af0a43
hxbq <1078662231@qq.com>
2025-10-24 12:18:12 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,14 +1,14 @@
 //202510303232
 //1078662231@qq.com
 //何熙
-#include <stdio.h>
+#include<stdio.h>
 int main()
-{  
+{
     float C = 0;
     //float T = C*9/5+32;
     scanf ("%f",&C);
     float T = C*9/5+32;
     printf("%.1f\n",T);
-    printf("Hello World\n");
+    printf("hello world\n");
     return 0;
 }
