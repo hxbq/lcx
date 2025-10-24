de213c9229f8b739a912657fdf6e372e7e9eaa59
hxbq <1078662231@qq.com>
2025-10-24 11:12:22 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510303232
+//1078662231@qq.com
+//何熙
 #include <stdio.h>
 int main()
-{
-    printf("Hello World");
+{   float C = 0;
+    //float T = C*9/5+32;
+    scanf ("%f",&C);
+    float T = C*9/5+32;
+    printf("%.1f\n",T);
+    printf("Hello World\n");
     return 0;
 }
