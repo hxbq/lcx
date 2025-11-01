bfde73a118e9187d3f629230717aa7aa4d9de332
hxbq <1078662231@qq.com>
2025-11-01 16:36:34 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,13 +2,40 @@
 //1078662231@qq.com
 //何熙
 #include<stdio.h>
+int isPrime(int num)
+{
+    if(num <= 1)
+    {
+        return 0;
+    }
+    if(num == 2)
+    {
+        return 1;
+    }
+    if(num % 2 == 0 )
+    {
+        return 0;
+    }
+    for(int i = 3; i*i <= num;i += 2 )
+    {
+        if(num % i == 0)
+       {
+            return 0;
+        }
+        return 1;
+    }
+}
 int main()
-{ 
-    float C = 0;
-    //float T = C*9/5+32;
-    scanf ("%f",&C);
-    float T = C*9/5+32;
-    printf("%.1f\n",T);
-    printf("hello world\n");
+{
+    int n;
+    scanf ("%d",&n);
+    if(isPrime(n))
+    {
+        printf("密钥安全，密码设置成功");
+    }
+    else
+    {
+        printf("密钥不安全，请重新输入");
+    }
     return 0;
 }
