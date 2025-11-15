cd4536284dc143796e839e759f4fbdd1a6ecc4f7
hxbq <1078662231@qq.com>
2025-11-15 12:49:36 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,33 +1,31 @@
-//202510303232
+///202510303232
 //1078662231@qq.com
 //何熙
 #include<stdio.h>
+int arrayone(int arr[],int le)
+{
+    int result1 = 0;
+    for(int i = 0;i < le;i++)
+    {
+        result1 += arr[i];
+    }
+    return result1;
+}
+int arraytwo(int arr[],int le)
+{
+    int result2 = 1;
+    for(int i = 0;i < le;i++)
+    {
+        result2 *= arr[i];
+    }
+    return result2;
+}
 int main()
 {
     int arr[5];
-    int count = 0;
-    while(count < 5)
-    {
-        int num;
-        printf("请输入第%d个数字:",count + 1);
-        scanf("%d",&num);
-        if(num % 2 == 0)
-        {
-            arr[count] = num;
-            count++;
-        }
-        else
-        {
-            printf("输入为奇数，请重新输入\n");
-        }
-    }
-    for(int i = 0;i < 5;i++)
-    {
-        printf("%d",arr[i]);
-        if(i != 4)
-        {
-            printf(" ");
-        }
-    }
+    scanf("%d %d %d %d %d",&arr[0],&arr[1],&arr[2],&arr[3],&arr[4]);
+    int one = arrayone(arr,5);
+    int two = arraytwo(arr,5);
+    printf("%d %d",one,two);
     return 0;
-}    
+}
