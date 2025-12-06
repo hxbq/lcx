b30a5640bd93a028673a71c2e025d243f148f4a9
hxbq <1078662231@qq.com>
2025-12-06 13:51:24 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,30 +2,18 @@
 //1078662231@qq.com
 //何熙
 #include<stdio.h>
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
-    int arr[10];
-    int i ,j ,temp;
-    for(i = 0;i < 10;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for(i = 0;i < 9;i++)
-    {
-        for(j = 0;j < 9 - i;j++)
-        {
-            if(arr[j] > arr[j + 1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for(i = 0;i < 10;i++)
-    {
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
+    int x, y;
+    scanf("%d %d",&x,&y);
+    swap(&x,&y);
+    printf("%d %d",x,y);
     return 0;
 }
